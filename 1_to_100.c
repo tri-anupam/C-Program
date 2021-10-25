9bbc1b0472164dad6ec49541169bba3cd47ce0be
Anupam Tripathi <arvindtri.70@gmail.com>
2021-10-25 22:32:09 +0530

    new program is added.

---
+++ 1_to_100.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+// -----This program is goes to print 1 to 100 number without using any number in program-----------
+int main()
+{
+    int one='a'/'a';
+    char s[]="..........";
+    int last=strlen(s);
+    for(int i=one;i<=(last*last);i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
